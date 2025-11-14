7aaf6b6c6883659f17ce52481ccabc380a993519
ellay21 <mesudmelaku1@gmail.com>
2025-11-14 15:39:26 +0200

    feat(cli): Add fluctuations analysis task

---
+++ src/tasks/run-fluctuations/include/run-fluctuations-aux.h
@@ -0,0 +1,571 @@
+#ifndef RUNFLUCTUATIONSAUX_H
+#define RUNFLUCTUATIONSAUX_H
+
+#include <fstream>
+#include <string>
+#include <functional>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <sstream>
+#include <map>
+#include <ctime>
+
+
+#ifdef __linux__
+#include <sys/stat.h>
+#endif
+
+#include "MDSystem.h"
+#include "NumberStatistics.h"
+
+#include "../../auxiliary/time-average-aux.h"
+
+
+struct RunFluctuationsParameters {
+  // Output file names prefix
+  std::string output_prefix;
+
+  // Numeric parameters
+  std::map<std::string, double> parameters;
+
+  // Default constructor with the default values
+  RunFluctuationsParameters(const std::string& input_file = "") :
+    output_prefix("run"),
+    parameters({
+      {"N",           400},        // the number of particles
+      {"T*",          1.4},        // the temperature, ignored if the microcanonical ensemble is used
+      //{"u*",          1.4},        // the energy per particle, ignored if the canonical ensemble is used
+      {"u*",          1.708},        // the energy per particle, ignored if the canonical ensemble is used
+      {"rho*",        0.60},       // the particle density
+      {"teq",         10.},        // the equilibration time time
+      {"tfin",        1000.},      // the maximum time, if negative, the calculation goes on indefinitely until stopped
+      {"dt*",         0.004},      // the integration time step
+      {"canonical",   1},          // the ensemble: 0 - microcanonical, 1 - canonical
+      {"subvolume_spacing", 0.05}, // the spacing in the values of the subvolume fractions
+      {"useCUDA",     1}           // if available, use CUDA GPU
+      })
+  {
+    if (input_file != "")
+      ReadParametersFromFile(input_file);
+  }
+
+  void ReadParametersFromFile(const std::string& filename) {
+    std::ifstream fin(filename);
+    if (fin.is_open()) {
+      std::cout << "Reading input parameters from file " << filename << "\n";
+      std::string var;
+      double val;
+
+      while (fin >> var) {
+        if (var.size() == 0 || var[0] == '#') {
+          fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+          continue;
+        }
+
+        std::cout << "Reading input parameter " << var << " = ";
+
+        if (var == "output_prefix") {
+          fin >> output_prefix;
+          std::cout << output_prefix << std::endl;
+          continue;
+        }
+
+        fin >> val;
+        std::cout << val << std::endl;
+
+        parameters[var] = val;
+      }
+    }
+    else {
+      std::cout << "Cannot open parameters file!" << "\n";
+    }
+    std::cout.flush();
+  }
+
+  void OutputParameters() {
+    const int tabsize = 25;
+
+    std::cout << "Lennard-Jones Molecular Dynamics fluctuations run parameter list:" << "\n";
+
+    for (auto& el : parameters) {
+      std::cout << std::setw(tabsize) << el.first << " = " << el.second << "\n";
+    }
+
+    std::cout << std::setw(tabsize) << "output_prefix" << " = " << output_prefix << "\n";
+
+    std::cout << std::endl;
+  }
+
+  std::string GetFullPrefix() {
+    std::string ret = output_prefix;
+
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    std::stringstream ss;
+    ss << std::put_time(&tm, "%d-%m-%Y-T%H-%M-%S");
+    ss << ".N" << parameters["N"];
+    if (std::lround(parameters["canonical"]))
+      ss << ".Tst" << parameters["T*"];
+    else
+      ss << ".ust" << parameters["u*"];
+
+    ss << ".rhost" << parameters["rho*"];
+
+    ret += "." + ss.str();
+
+    return ret;
+  }
+
+};
+
+namespace RunFluctuationsFunctions {
+
+  // Calculate the number of particles in a subsystem at a given moment
+  // type: 0 - |x| < xcut, 1 - |y| < ycut, 2 - |z| < zcut, 3 - cube around the center
+  // fraction: fraction of the total volume
+  static int GetNSubsystem(const MDSystem& syst, double fraction = 0.5, int type = 3) {
+    double L = syst.L;
+    int N = syst.m_config.N;
+
+    double tL = L * pow(fraction, 1. / 3.);
+
+    int ret = 0;
+
+    for (int i = 0; i < 4 * N; i += 4) {
+      double x = syst.h_Pos[i];
+      double y = syst.h_Pos[i + 1];
+      double z = syst.h_Pos[i + 2];
+      if (type == 3) {
+        double dx = (x - 0.5 * L);
+        double dy = (y - 0.5 * L);
+        double dz = (z - 0.5 * L);
+
+        if (std::abs(dx) < 0.5 * tL && std::abs(dy) < 0.5 * tL && std::abs(dz) < 0.5 * tL)
+          ret++;
+      }
+      else {
+        double coord = x;
+        if (type == 1)
+          coord = y;
+        else if (type == 2)
+          coord = z;
+        if (coord < fraction * L)
+          ret++;
+      }
+    }
+
+    return ret;
+  }
+
+  // Calculate the number of particles in a momentum space subsystem at a given time moment
+  // type: 0 - |vx| < vxcut, 1 - |vy| < vycut, 2 - |vz| < vzcut
+  // fraction: fraction of the total volume
+  static int GetNsubVz(const MDSystem& syst, double vcut = 0.5, int type = 0) {
+    int N = syst.m_config.N;
+
+    int ret = 0;
+
+    for (int i = 0; i < 4 * N; i += 4) {
+      double vx = syst.h_Vel[i];
+      double vy = syst.h_Vel[i + 1];
+      double vz = syst.h_Vel[i + 2];
+
+      double v = vx;
+      if (type == 1)
+        v = vy;
+      else if (type == 2)
+        v = vz;
+
+      if (std::abs(v) < vcut)
+        ret++;
+    }
+
+    return ret;
+  }
+
+  // Calculate the number of particles in subsystems at a given time moment
+  // type: 0 - |x| < xcut, 1 - |y| < ycut, 2 - |z| < zcut, 3 - cube around the center
+  // alpha_step: step in the fractions of the total volume, from 0 to 1
+  static std::vector<int> GetNSubsystemBatch(const MDSystem& syst, double alpha_step = 0.05, int type = 3) {
+    double L = syst.L;
+    int N = syst.m_config.N;
+
+    std::vector<double> tLs;
+    std::vector<int> cnts;
+    
+    for (double alpha = alpha_step; alpha < 1. - 1.e-9; alpha += alpha_step) {
+      if (type == 3) {
+        tLs.push_back(L * pow(alpha, 1. / 3.));
+      }
+      cnts.push_back(0);
+    }
+
+    for (int i = 0; i < 4 * N; i += 4) {
+      double x = syst.h_Pos[i];
+      double y = syst.h_Pos[i + 1];
+      double z = syst.h_Pos[i + 2];
+      if (type == 3) {
+        double dx = (x - 0.5 * L);
+        double dy = (y - 0.5 * L);
+        double dz = (z - 0.5 * L);
+
+        //if (abs(dx) < 0.5 * tL && abs(dy) < 0.5 * tL && abs(dz) < 0.5 * tL)
+        //  ret++;
+        int tindx = std::distance(tLs.begin(), upper_bound(tLs.begin(), tLs.end(), std::abs(dx) / 0.5));
+        int tindy = std::distance(tLs.begin(), upper_bound(tLs.begin(), tLs.end(), std::abs(dy) / 0.5));
+        int tindz = std::distance(tLs.begin(), upper_bound(tLs.begin(), tLs.end(), std::abs(dz) / 0.5));
+        int tind = std::max({ tindx,tindy,tindz });
+        if (tind < cnts.size())
+          cnts[tind]++;
+      }
+      else {
+        double coord = x;
+        if (type == 1)
+          coord = y;
+        else if (type == 2)
+          coord = z;
+
+        coord /= L;
+        int tind = (int)(coord / alpha_step);
+        if (tind < cnts.size())
+          cnts[tind]++;
+      }
+    }
+
+    for (int i = 1; i < cnts.size(); ++i)
+      cnts[i] += cnts[i - 1];
+
+    return cnts;
+  }
+
+  // Calculate the number of particles in a momentum space subsystem at a given time moment
+  // type: 0 - |vx| < vxcut, 1 - |vy| < vycut, 2 - |vz| < vzcut
+  // alpha_step: step in the fractions of the total volume, from 0 to 1
+  static std::vector<int> GetNsubVzBatch(const MDSystem& syst, double vcut_max, double alpha_step = 0.05, int type = 0) {
+    int N = syst.m_config.N;
+
+    std::vector<int> cnts;
+
+    for (double alpha = alpha_step; alpha < 1. + 1.e-9; alpha += alpha_step) {
+      cnts.push_back(0);
+    }
+
+    int ret = 0;
+
+    for (int i = 0; i < 4 * N; i += 4) {
+      double vx = syst.h_Vel[i];
+      double vy = syst.h_Vel[i + 1];
+      double vz = syst.h_Vel[i + 2];
+
+      double v = vx;
+      if (type == 1)
+        v = vy;
+      else if (type == 2)
+        v = vz;
+
+      v /= vcut_max;
+      int tind = (int)(std::abs(v) / alpha_step);
+      if (tind < cnts.size())
+        cnts[tind]++;
+    }
+
+    for (int i = 1; i < cnts.size(); ++i)
+      cnts[i] += cnts[i - 1];
+
+    return cnts;
+  }
+
+  // Get the average velocities
+  // Returns a vector {<vx>, <vy>, <vz>}
+  static std::vector<double> GetAvVel(const MDSystem& syst) {
+    std::vector<double> ret(3, 0.);
+    for (int i = 0; i < 4 * syst.m_config.N; i += 4) {
+      ret[0] += syst.h_Vel[i];
+      ret[1] += syst.h_Vel[i + 1];
+      ret[2] += syst.h_Vel[i + 2];
+    }
+    for (int i = 0; i < 3; ++i)
+      ret[i] /= syst.m_config.N;
+    return ret;
+  }
+
+
+  // Radial distribution function time average
+  struct RDF_average {
+    double max_r;
+    double dr;
+    int iters_rdf;
+    SplineFunction Gr;
+
+    RDF_average(double maxr = 5.0, double ddr = 0.03) : max_r(maxr), dr(ddr), iters_rdf(0) {}
+
+    void AddTimeStep(MDSystem& syst) {
+      SplineFunction crdf = syst.RDF(max_r, dr);
+      if (iters_rdf == 0)
+        Gr = crdf;
+      else
+      {
+        for (int i = 0; i < crdf.vals.size(); ++i)
+          Gr.vals[i].second = (Gr.vals[i].second * iters_rdf + crdf.vals[i].second) / (iters_rdf + 1);
+      }
+      iters_rdf++;
+    }
+
+    void PrintToFile(const std::string& filename) {
+      std::ofstream fout(filename);
+      if (fout.is_open()) {
+        fout << std::setw(15) << "r*" << " ";
+        fout << std::setw(15) << "G(r)" << " ";
+      }
+      fout << std::endl;
+
+      for (int i = 0; i < Gr.vals.size(); ++i) {
+        fout << std::setw(15) << Gr.vals[i].first << " ";
+        fout << std::setw(15) << Gr.vals[i].second << " ";
+        fout << std::endl;
+      }
+
+      fout.close();
+    }
+  };
+
+  // Time average for coordinate space fluctuations
+  struct CoordFlucsAverage {
+    int type;
+    double alpha_step;
+    //std::vector<SampleMoments::NumberStatistics> stats;
+    std::vector<double> alphas, totsN, totsN2;
+    //std::vector<double> lastsN;
+    //std::vector<double> corrsN;
+    std::vector<TimeAverage> stats;
+    int iters;
+
+    CoordFlucsAverage(int type_ = 0, double alphastep = 0.05) :
+      type(type_),
+      alpha_step(alphastep),
+      iters(0) 
+    {
+      totsN = totsN2 = std::vector<double>(0);
+      for (double alpha = alphastep; alpha <= 1. - 1.e-9; alpha += alpha_step) {
+        totsN.push_back(0.);
+        totsN2.push_back(0.);
+        //lastsN.push_back(0.);
+        //corrsN.push_back(0.);
+        alphas.push_back(alpha);
+        //stats.push_back(SampleMoments::NumberStatistics());
+        stats.push_back(TimeAverage());
+      }
+    }
+
+    void AddTimeStep(const MDSystem& syst) {
+      std::vector<int> cnts = GetNSubsystemBatch(syst, alpha_step, type);
+      for (int ia = 0; ia < alphas.size(); ++ia) {
+        double alpha = alphas[ia];
+        //int Nsub = GetNSubsystem(syst, alpha, type);
+        int Nsub = cnts[ia];
+        totsN[ia]  += static_cast<double>(Nsub);
+        totsN2[ia] += static_cast<double>(Nsub) * static_cast<double>(Nsub);
+
+        stats[ia].AddObservation(Nsub);
+
+        //if (iters > 0) {
+        //  corrsN[ia] += lastsN[ia] * static_cast<double>(Nsub);
+        //}
+
+        //lastsN[ia] = static_cast<double>(Nsub);
+
+        /*if (ia == alphas.size() - 1) {
+          std::cout << Nsub << " " << cnts[ia] << " ; ";
+        }*/
+      }
+      iters++;
+    }
+
+    void PrintToFile(const std::string& filename) {
+      std::ofstream fout(filename);
+      if (fout.is_open()) {
+        fout << std::setw(15) << "alpha" << " ";
+        fout << std::setw(15) << "<N>" << " ";
+        fout << std::setw(15) << "w[N]" << " ";
+        fout << std::setw(15) << "w[N]/(1-alpha)" << " ";
+        fout << std::setw(15) << "error" << " ";
+        fout << std::setw(15) << "s" << " ";
+      }
+      fout << std::endl;
+
+      for (int ia = 0; ia < alphas.size(); ++ia) {
+        double alpha = alphas[ia];
+
+        fout << std::setw(15) << alpha << " ";
+
+        double Nav  = totsN[ia] / iters;
+        double N2av = totsN2[ia] / iters;
+        double w = (N2av - Nav * Nav) / Nav;
+
+        Nav = stats[ia].GetMean();
+        double Nvar = stats[ia].stats.GetVariance();
+        w = stats[ia].stats.GetScaledVariance();// stats[ia].GetVariance() / stats[ia].GetMean();
+        //double Ncorrav = corrsN[ia] / (iters - 1) - Nav * Nav;
+        //double s = 2./ log(Nvar / Ncorrav);
+        //if (s < 0.)
+        //  s = 1.;
+        double s = stats[ia].GetS();
+
+        fout << std::setw(15) << Nav << " ";
+        fout << std::setw(15) << w << " ";
+        fout << std::setw(15) << w / (1. - alpha) << " ";
+        fout << std::setw(15) << stats[ia].stats.GetScaledVarianceError() * sqrt(s) / (1. - alpha) << " ";
+        fout << std::setw(15) << s << " ";
+        fout << std::endl;
+      }
+
+      fout.close();
+    }
+  };
+
+
+  struct MomentumFlucsAverage {
+    int type;
+    double step;
+    const double Vfactor = 3.0;
+    std::vector<double> vcuts, totsN, totsN2;
+    //std::vector<SampleMoments::NumberStatistics> stats;
+    //std::vector<double> lastsN;
+    //std::vector<double> corrsN;
+    std::vector<TimeAverage> stats;
+    int iters;
+    int totN;
+
+    MomentumFlucsAverage(const MDSystem& syst, int type_ = 0, double alphastep = 0.05) :
+      type(type_),
+      step(alphastep),
+      iters(0)
+    {
+      totsN = totsN2 = std::vector<double>(0);
+      double Tst = syst.m_config.T0;
+      totN = syst.m_config.N;
+      for (double alpha = alphastep; alpha <= 1. + 1.e-9; alpha += step) {
+        double vcut = sqrt(Tst) * alpha * Vfactor;
+        totsN.push_back(0.);
+        totsN2.push_back(0.);
+        vcuts.push_back(vcut);
+        //lastsN.push_back(0.);
+        //corrsN.push_back(0.);
+        //stats.push_back(SampleMoments::NumberStatistics());
+        stats.push_back(TimeAverage());
+      }
+    }
+
+    void AddTimeStep(const MDSystem& syst) {
+      std::vector<int> cnts = GetNsubVzBatch(syst, sqrt(syst.m_config.T0) * Vfactor, step, type);
+      for (int ia = 0; ia < vcuts.size(); ++ia) {
+        double vcut = vcuts[ia];
+        //int Nsub = GetNsubVz(syst, vcut, type);
+        int Nsub = cnts[ia];
+        totsN[ia] += static_cast<double>(Nsub);
+        totsN2[ia] += static_cast<double>(Nsub) * static_cast<double>(Nsub);
+
+        stats[ia].AddObservation(Nsub);
+
+        //if (iters > 0) {
+        //  corrsN[ia] += lastsN[ia] * static_cast<double>(Nsub);
+        //}
+
+        //lastsN[ia] = static_cast<double>(Nsub);
+
+        //if (ia == vcuts.size() - 1) {
+        //  std::cout << Nsub << " " << cnts[ia] << " ; ";
+        //}
+      }
+      iters++;
+    }
+
+    void PrintToFile(const std::string& filename) {
+      std::ofstream fout(filename);
+      if (fout.is_open()) {
+        fout << std::setw(15) << "vcut" << " ";
+        fout << std::setw(15) << "<N>" << " ";
+        fout << std::setw(15) << "alpha" << " ";
+        fout << std::setw(15) << "w[N]" << " ";
+        fout << std::setw(15) << "w[N]/(1-alpha)" << " ";
+        fout << std::setw(15) << "error" << " ";
+        fout << std::setw(15) << "s" << " ";
+      }
+      fout << std::endl;
+
+      for (int ia = 0; ia < vcuts.size(); ++ia) {
+        double vcut = vcuts[ia];
+
+        fout << std::setw(15) << vcut << " ";
+
+        double Nav = totsN[ia] / iters;
+        double N2av = totsN2[ia] / iters;
+        double w = (N2av - Nav * Nav) / Nav;
+
+        Nav = stats[ia].GetMean();
+        double Nvar = stats[ia].stats.GetVariance();
+        w = stats[ia].stats.GetScaledVariance();// stats[ia].GetVariance() / stats[ia].GetMean();
+        //double Ncorrav = corrsN[ia] / (iters - 1) - Nav * Nav;
+        //double s = 2. / log(Nvar / Ncorrav);
+        //if (s < 0.)
+        //  s = 1.;
+        double s = stats[ia].GetS();
+
+        fout << std::setw(15) << Nav << " ";
+        double alpha = Nav / totN;
+        fout << std::setw(15) << alpha << " ";
+        fout << std::setw(15) << w << " ";
+        fout << std::setw(15) << w / (1. - alpha) << " ";
+        fout << std::setw(15) << stats[ia].stats.GetScaledVarianceError() * sqrt(s) / (1. - alpha) << " ";
+        fout << std::setw(15) << s << " ";
+        fout << std::endl;
+      }
+
+      fout.close();
+    }
+  };
+}
+
+namespace StatisticalInefficiency {
+
+  double CalculatateStatisticalInefficiency(const std::vector<double> vals, int tau_b) {
+    int nb = 0;
+    int tau_run = 0;
+    double mean_N = 0., mean_N2 = 0.;
+    double mean_NBs = 0., mean_N2Bs = 0.;
+
+    while (tau_run + tau_b < vals.size()) {
+      double mean_NB = 0.;
+      for (int ib = 0; ib < tau_b; ib++) {
+        mean_N  += vals[tau_run + ib];
+        mean_N2 += vals[tau_run + ib] * vals[tau_run + ib];
+        mean_NB += vals[tau_run + ib];
+      }
+
+      mean_NB /= tau_b;
+
+      mean_NBs += mean_NB;
+      mean_N2Bs += mean_NB * mean_NB;
+
+      tau_run += tau_b;
+      nb++;
+    }
+
+    mean_N /= tau_run;
+    mean_N2 /= tau_run;
+
+
+    mean_NBs /= nb;
+    mean_N2Bs /= nb;
+
+    double sigma_A = mean_N2 - mean_N * mean_N;
+    double sigma_AB = mean_N2Bs - mean_NBs * mean_NBs;
+
+    return tau_b * sigma_AB / sigma_A;
+  }
+
+}
+
+#endif
---
+++ src/tasks/run-fluctuations/run-fluctuations.cpp
@@ -0,0 +1,203 @@
+#include "include/run-fluctuations-aux.h"
+#include "NumberStatistics.h"
+#ifdef USE_CUDA_TOOLKIT
+#include "cuda_runtime.h"
+#endif
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+	RunFluctuationsParameters params;
+
+	if (argc > 1) {
+		cout << "Reading parameters from file " << string(argv[1]) << endl;
+		params.ReadParametersFromFile(string(argv[1]));
+	}
+	params.output_prefix = params.GetFullPrefix();
+
+	// check CUDA
+	{
+		int deviceCount = 0;
+#ifdef USE_CUDA_TOOLKIT
+		cudaGetDeviceCount(&deviceCount);
+#endif
+
+		if (deviceCount == 0 && lround(params.parameters["useCUDA"])) {
+			std::cout << "Could not find a CUDA device! Calculations will be performed on CPU\n";
+			params.parameters["useCUDA"] = 0;
+		}
+	}
+
+	// Print all the run parameters
+	params.OutputParameters();
+
+	//cout << params.GetFullPrefix() << endl;
+
+	int N = lround(params.parameters["N"]);
+	double rhost = params.parameters["rho*"];
+	double Tst = params.parameters["T*"];
+	double ust = params.parameters["u*"];
+
+    vector<double> fracs;
+	{
+		double dfr = params.parameters["subvolume_spacing"];
+		for (double tfr = dfr; tfr <= 1.; tfr += dfr)
+			fracs.push_back(tfr);
+	}
+
+	// Time parameters
+	double dt = params.parameters["dt*"];
+	double teq  = params.parameters["teq"];
+	double tfin = params.parameters["tfin"];
+
+	MDSystem::MDSystemConfiguration config;
+	config.N = N;
+	config.T0 = Tst;
+	config.rho = rhost; 
+	config.useCUDA = lround(params.parameters["useCUDA"]);
+	config.canonical = lround(params.parameters["canonical"]);
+
+	MDSystem syst(config);
+	syst.Reinitialize(config);
+	if (!config.canonical) {
+		syst.RenormalizeVelocitiesToEnergy(ust);
+	}
+
+
+	// Create counters
+	double dalpha = params.parameters["subvolume_spacing"];
+	RunFluctuationsFunctions::RDF_average RDF(5.0, 0.03);
+	RunFluctuationsFunctions::CoordFlucsAverage flucs_coord_X(0, dalpha);
+	RunFluctuationsFunctions::CoordFlucsAverage flucs_coord_Y(1, dalpha);
+	RunFluctuationsFunctions::CoordFlucsAverage flucs_coord_Z(2, dalpha);
+	RunFluctuationsFunctions::CoordFlucsAverage flucs_coord_cube(3, dalpha);
+	RunFluctuationsFunctions::MomentumFlucsAverage flucs_velocity_z(syst, 2, dalpha);
+
+
+	double t = 0.;
+
+	// Equilibration phase
+	while (t < teq) {
+		syst.Integrate(dt);
+		t += dt;
+	}
+
+	// Prepare output
+	cout << setw(15) << "t*"   << " ";
+	cout << setw(15) << "u*"   << " ";
+	cout << setw(15) << "T*"   << " ";
+	cout << setw(15) << "Z"    << " ";
+	cout << setw(15) << "<u*>" << " ";
+	cout << setw(15) << "<T*>" << " ";
+	cout << setw(15) << "<Z>"  << " ";
+	cout << setw(15) << "<w>/(1-x)" << " ";
+	cout << endl;
+
+	ofstream fout(params.output_prefix + string(".TimeDep.txt"));
+	fout << setw(15) << "t*" << " ";
+	fout << setw(15) << "u*" << " ";
+	fout << setw(15) << "T*" << " ";
+	fout << setw(15) << "Z" << " ";
+	fout << setw(15) << "<u*>" << " ";
+	fout << setw(15) << "<T*>" << " ";
+	fout << setw(15) << "<Z>" << " ";
+	fout << setw(15) << "<wx>/(1-x)" << " ";
+	fout << setw(15) << "<wy>/(1-x)" << " ";
+	fout << setw(15) << "<wz>/(1-x)" << " ";
+	fout << setw(15) << "<vx>" << " ";
+	fout << setw(15) << "<vy>" << " ";
+	fout << setw(15) << "<vz>" << " ";
+	fout << endl;
+
+	// Production phase
+	double tot_N = 0.0, tot_N2 = 0.0;  // Fluctuations at \alpha = 0.5
+	int totIters = 0;  // Total iterations
+	syst.resetAveraging();
+	while (t < tfin || tfin < 0.) {
+		syst.Integrate(dt);
+		t += dt;
+		totIters++;
+
+		// Update statistics
+		{
+			RDF.AddTimeStep(syst);
+			flucs_coord_X.AddTimeStep(syst);
+			flucs_coord_Y.AddTimeStep(syst);
+			flucs_coord_Z.AddTimeStep(syst);
+			flucs_coord_cube.AddTimeStep(syst);
+			flucs_velocity_z.AddTimeStep(syst);
+		}
+
+		int tN = RunFluctuationsFunctions::GetNSubsystem(syst, 0.5, 2);
+		tot_N += tN;
+		tot_N2 += tN * tN;
+
+		// Output
+		if (totIters % 1000 == 0) {
+			cout << setw(15) << t << " ";
+			cout << setw(15) << syst.U / syst.m_config.N << " ";
+			cout << setw(15) << syst.T << " ";
+			cout << setw(15) << syst.P / (syst.m_config.rho * syst.T) << " ";
+			cout << setw(15) << syst.av_U_tot / syst.av_iters / syst.m_config.N << " ";
+			double Tav = syst.av_T_tot / syst.av_iters;
+			cout << setw(15) << Tav << " ";
+			cout << setw(15) << syst.av_p_tot / syst.av_iters / (syst.m_config.rho * Tav) << " ";
+
+			double Nav = tot_N / totIters;
+			double N2av = tot_N2 / totIters;
+
+
+			cout << setw(15) << (N2av - Nav * Nav) / Nav / (1. - 0.5) << " ";
+			cout << endl;
+
+
+			fout << setw(15) << t << " ";
+			fout << setw(15) << syst.U / syst.m_config.N << " ";
+			fout << setw(15) << syst.T << " ";
+			fout << setw(15) << syst.P / (syst.m_config.rho * syst.T) << " ";
+			fout << setw(15) << syst.av_U_tot / syst.av_iters / syst.m_config.N << " ";
+			fout << setw(15) << Tav << " ";
+			fout << setw(15) << syst.av_p_tot / syst.av_iters / (syst.m_config.rho * Tav) << " ";
+			//fout << setw(15) << (N2av - Nav * Nav) / Nav / (1. - 0.5) << " ";
+			{
+				RunFluctuationsFunctions::CoordFlucsAverage *flucs;
+				for(int ic = 0; ic < 3; ++ic) {
+					if (ic == 0)
+						flucs = &flucs_coord_X;
+					else if (ic == 1)
+						flucs = &flucs_coord_Y;
+					else
+						flucs = &flucs_coord_Z;
+
+					int ti = (*flucs).alphas.size() / 2;
+					double alpha = (*flucs).alphas[ti];
+					double tNav  = (*flucs).totsN[ti]  / (*flucs).iters;
+					double tN2av = (*flucs).totsN2[ti] / (*flucs).iters;
+					double tw = (tN2av - tNav * tNav) / tNav;
+					fout << setw(15) << tw / (1. - alpha) << " ";
+				}
+			}
+
+
+			auto velo = RunFluctuationsFunctions::GetAvVel(syst);
+			fout << setw(15) << velo[0] << " ";
+			fout << setw(15) << velo[1] << " ";
+			fout << setw(15) << velo[2] << " ";
+			fout << endl;
+			fout.flush();
+
+			// Other outputs
+			RDF.PrintToFile(params.output_prefix + string(".RDF.dat"));
+			flucs_coord_X.PrintToFile(params.output_prefix + string(".flucsX.dat"));
+			flucs_coord_Y.PrintToFile(params.output_prefix + string(".flucsY.dat"));
+			flucs_coord_Z.PrintToFile(params.output_prefix + string(".flucsZ.dat"));
+			flucs_coord_cube.PrintToFile(params.output_prefix + string(".flucsCube.dat"));
+			flucs_velocity_z.PrintToFile(params.output_prefix + string(".flucsVz.dat"));
+		}
+	}
+
+  return 0;
+}
+ 
